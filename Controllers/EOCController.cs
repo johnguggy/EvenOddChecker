b2b7d433ef58375903ccd736c579e4ba6afbb0fc
johnguggy <42472160+johnguggy@users.noreply.github.com>
2018-10-25 15:20:59 -0400

    Add files via upload

---
+++ Controllers/EOCController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvenOddCheckerMVC.Controllers
+{
+    public class EOCController : Controller
+    {
+        // GET: EOC
+        public ActionResult Index()
+        {
+            Random random = new Random();
+            int num1 = random.Next(100);
+            ViewBag.num1 = num1;
+            return View();
+        }
+    }
+}
